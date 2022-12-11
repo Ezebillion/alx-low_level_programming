747247768c6f464ba29c459baded71abfb829706
Ezebillion <ebizzy4money@gmail.com>
2022-12-11 09:29:38 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return; always 0 'Success'
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	for (low = 'A'; low <= 'Z'; low++)
+		putchar9low);
+	putchar('\n');
+
+	return (0);
+}
