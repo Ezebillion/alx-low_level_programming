22599015474d07070868ba6c15cd9387020377e0
Ezebillion <ebizzy4money@gmail.com>
2022-12-11 13:34:03 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,17 +5,17 @@
 /**
  * main - Entry Point
  *
- *Retrun: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char low, e, q;
+	char low. e. q;
 
 	e = 'e';
 	q = 'q';
 
-	for (low = 'a'; low < 'z'; low++)
+	for (low = 'a'; low <= 'z'; low++)
 	{
 		if (low != e && low != q)
 			putchar(low);
