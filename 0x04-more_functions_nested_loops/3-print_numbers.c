bfb8622e191323c17304684bd2cc91ed89fd8840
Ezebillion <ebizzy4money@gmail.com>
2022-12-16 17:55:51 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers
+ */
+void print_numbers(void)
+{
+	char ln;
+
+	for (ln = '0'; ln <= '9'; ln++)
+		_putchar(ln);
+
+	_putchar('\n');
+}
