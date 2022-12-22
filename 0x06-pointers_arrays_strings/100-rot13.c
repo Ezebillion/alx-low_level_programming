c2d26305e342e97a320dc09ac1db5cfff45fb18f
Ezebillion <ebizzy4money@gmail.com>
2022-12-22 12:12:14 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,33 +1,30 @@
 #include "main.h"
-
+i
 /**
  * rot13 - encodes a string using rot13
- * @s: input string
- * Return: the pointer to dest
+ * @str: the string targeted
+ * Return: returns the encoded string
  */
 
-char *rot13(char *s)
+char *rot13(char *str)
 {
-	int count = 0, i;
+	int index1, index2;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
 
-	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-
-	while (*(s = count) != '\0')
+	while (str[++index1])
 	{
-		for (i = 0; i < 52; i++)
+		for (index2 = 0; index2 < 52; index2++)
 		{
-			if (*(s + count) == alphabet[i])
+			if (str[index1] == alphabet[index2])
 			{
-				*(s + count) = rot13[i];
-
+				str[index1] = rot13key[index2];
 				break;
 			}
 		}
-		count++;
-
 	}
-
-	return (s);
+	return (str);
 }
+
+
