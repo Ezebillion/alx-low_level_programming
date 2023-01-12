33e565fe9379aa3f15be8b3c01d5ae7275e0fa13
Ezebillion <ebizzy4money@gmail.com>
2023-01-12 13:23:49 -0800

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * malloc_checked - allocates memory using malloc
  * @b: size
@@ -10,7 +9,8 @@
 void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b * sizeof(int));
+
 	if (ptr == NULL)
-		exit (98);
+		exit(98);
 	return (ptr);
 }
